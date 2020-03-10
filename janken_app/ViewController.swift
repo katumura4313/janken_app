a76571c231852f2940f44ce8d605b988ce6dc5a7
katsumura4313 <n4ru4313@gmail.com>
2020-03-10 20:57:58 +0900

    ジャンケンアプリの作成

---
+++ janken_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  janken_app
+//
+//  Created by USER on 2020/03/10.
+//  Copyright © 2020 jp.go. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
